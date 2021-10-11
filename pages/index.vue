09a41b32b40ae80567c759cfbbf02833f4a357b1
HiepDo <bacodekiller@gmail.com>
2021-10-11 20:54:10 +0700

    feat: loading spin

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,14 @@
 <template>
-  <div class="grid grid-cols-2 gap-x-4 lg:grid-cols-4 mt-20 max-w-screen-xl mx-auto w-full px-4">
+  <div class="grid grid-cols-2 gap-x-4 lg:grid-cols-4 justify-items-center mt-20 max-w-screen-xl mx-auto w-full px-4">
     <Tutorial/>
     <spin-strech />
+    <loading-spin />
   </div>
 </template>
 
 <script>
+import LoadingSpin from '../components/LoadingSpin.vue'
 import SpinStrech from '../components/SpinStrech.vue'
 export default {
-  components: { SpinStrech },}
+  components: { SpinStrech, LoadingSpin },}
 </script>
